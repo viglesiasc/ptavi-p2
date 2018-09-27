85d15dbfd5682648e28cf63d1848cc82e69c3a1d
Victor <victoriglcue@gmail.com>
2018-09-27 17:54:11 +0200

    Cumpliendo PEP8

---
+++ calcplusplus.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+
+from calcoohija import CalculadoraHija
+Calc_Hija = CalculadoraHija()
+
+with open(sys.argv[1]) as fichero:
+    reader = csv.reader(fichero)
+    for linea in reader:
+        operandos = linea[1:]
+        if linea[0] == 'suma':
+            operando_aux = 0
+            for operado in operandos:
+                suma = Calc_Hija.plus(operando_aux, int(operado))
+                operando_aux = suma
+        elif linea[0] == 'resta':
+            operando_aux = int(operandos[0])
+            for operando in operandos[1:]:
+                operando_aux = Calc_Hija.minus(operando_aux, int(operando))
+                resta = operando_aux
+        elif linea[0] == 'multiplica':
+            operando_aux = 1
+            for operado in operandos:
+                producto = Calc_Hija.multiplicacion(operando_aux, int(operado))
+                operando_aux = producto
+        elif linea[0] == 'divide':
+            operando_aux = int(operandos[0])
+            for operando in operandos[1:]:
+                operando_aux = Calc_Hija.division(operando_aux, int(operando))
+                if int(operando) == 0:
+                    print('No se puede dividir por zero')
+                else:
+                    razon = operando_aux
+        else:
+            print('No se puede realizar la operacion')
+
+print("Suma = " + str(suma))
+print("Resta = " + str(resta))
+print("Multiplicacion = " + str(producto))
+print("Division = " + str(int(razon)))
