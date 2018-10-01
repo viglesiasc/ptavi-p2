7b5335cbff71a2c8ebc13e1168328929784fe933
Victor Iglesias Cuevas <viglesia@gamma27.aulas.gsyc.urjc.es>
2018-10-01 10:32:09 +0200

    Solucionado control de error al dividir por zero

--- calcplus.py
+++ calcplus.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from calcoohija import CalculadoraHija
 
 fich = open(sys.argv[1], 'r')
 lineas = fich.readlines()
 fich.close()
-
-from calcoohija import CalculadoraHija
-
 Calc_Hija = CalculadoraHija()
+
 for linea in lineas:
     elemento = linea.split(',')
     elemento[-1] = elemento[-1][:-1]
@@ -33,11 +32,12 @@ for linea in lineas:
     elif elemento[0] == 'divide':
         operando_aux = int(operandos[0])
         for operando in operandos[1:]:
-            operando_aux = Calc_Hija.division(operando_aux, int(operando))
-            if int(operando) == 0:
-                print('No se puede dividir por zero')
-            else:
+            try:
+                operando_aux = Calc_Hija.division(operando_aux, int(operando))
                 razon = operando_aux
+            except ZeroDivisionError:
+                sys.exit('Division by zero is not allowed')
+
     else:
 
         print('No se puede realizar la operacion')
@@ -45,4 +45,4 @@ for linea in lineas:
 print("Suma = " + str(suma))
 print("Resta = " + str(resta))
 print("Multiplicacion = " + str(producto))
-print("Division = " + str(int(razon)))
+print("Division = " + str(razon))
